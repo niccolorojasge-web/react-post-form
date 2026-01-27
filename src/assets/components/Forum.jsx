17e7adaaee1f1c7473402f137a8728395c8dc655
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-01-27 17:12:51 +0100

    checkbox

--- src/assets/components/Forum.jsx
+++ src/assets/components/Forum.jsx
@@ -1,41 +1,74 @@
 import { useState } from 'react'
 
-import './App.css'
+//import './App.css'
 
 function Forum() {
-  const [date, setDate] = useState({
-    author :"",
-    title: "",
-    body:"",
-    public :"false"
-  })
-  function handleform(e){
-   setDate({
-    ...date,
-   [e.target.name]: e.target.value
-   });
-  }
+    const [data, setData] = useState({
+        author: "",
+        title: "",
+        body: "",
+        public: false
+    })
+    function handleform(e) {
+        const value =
+        e.target.type ==="checkbox"?
+        e.target.checked : e.target.value;
+        setData({
+            ...data,
+            [e.target.name]:value,
+         });
+    }
 
-  return (
-    <div className='container'>
-     <div className='row'>
-        <div className='col'>
-            <form >
-             <div className='row'>
+    return (
+        <div className='container'>
+            <div className='row'>
                 <div className='col'>
-                    <input
-                    name="author"
-                    type="text"
-                    placeholder="author"
-                    value ={date.author} 
-                    onChange={handleform}/>
+                    <form>
+                        <div className='row'>
+                            <div className='col'>
+                                <input
+                                    name="author"
+                                    type="text"
+                                    placeholder="author"
+                                    value={data.author}
+                                    onChange={handleform}
+                                />
+                            </div>
+                            <div className='col'>
+                                <input
+                                    name="title"
+                                    type="text"
+                                    placeholder="title"
+                                    value={data.title}
+                                    onChange={handleform}
+                                />
+                            </div>
+                            <label htmlFor="">
+                                <textarea
+                                    name="author"
+                                    type="textarea"
+                                    placeholder="body"
+                                    value={data.body}
+                                    onChange={handleform}
+                                />
+                            </label>
+                            <div>
+                              
+                              <input type="checkbox"
+                              name="public"
+                              checked={data.public}
+                              onChange={handleform}
+                              />
+                              <label htmlFor="public">
+                                  post pubblic
+                              </label>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-             </div>
-            </form>
+            </div>
         </div>
-     </div>
-    </div>
-  )
+    )
 }
 
 export default Forum
