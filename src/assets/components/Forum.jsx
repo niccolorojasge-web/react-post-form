857f42275b3faef090efb90fc09ac43cfd5ec2cc
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-01-27 16:11:03 +0100

    author

--- src/assets/components/Forum.jsx
+++ src/assets/components/Forum.jsx
@@ -18,7 +18,22 @@ function Forum() {
 
   return (
     <div className='container'>
-
+     <div className='row'>
+        <div className='col'>
+            <form >
+             <div className='row'>
+                <div className='col'>
+                    <input
+                    name="author"
+                    type="text"
+                    placeholder="author"
+                    value ={date.author} 
+                    onChange={handleform}/>
+                </div>
+             </div>
+            </form>
+        </div>
+     </div>
     </div>
   )
 }
